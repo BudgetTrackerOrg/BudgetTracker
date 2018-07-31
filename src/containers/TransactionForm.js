ea96948ccc394c2523c21adf48b0d600e835e1fa
gregsakai <gregsakai@gmail.com>
2018-07-31 14:48:08 -0700

    Created containers directory - for Smart Components which will use Redux

---
+++ src/containers/TransactionForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import styles from '../styles/App.scss'
+
+import CategoryBox from '../components/Categories/CategoryBox'
+
+export class TransactionForm extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.form__heading}>{this.props.heading}</Text>
+
+                <View style={styles.categories}>
+                    <CategoryBox
+                        categoryIcon="home"
+                        categoryName="House/Rent"
+                    />
+                    <CategoryBox
+                        categoryIcon="car"
+                        categoryName="Car/Transit"
+                    />
+                    <CategoryBox
+                        categoryIcon="cutlery"
+                        categoryName="Food/Drink"
+                    />
+                    <CategoryBox
+                        categoryIcon="credit-card"
+                        categoryName="Payments/Bills"
+                    />
+                    <CategoryBox
+                        categoryIcon="film"
+                        categoryName="Entertainment"
+                    />
+                    <CategoryBox
+                        categoryIcon="shopping-cart"
+                        categoryName="Shopping"
+                    />
+                    <CategoryBox
+                        categoryIcon="graduation-cap"
+                        categoryName="Education"
+                    />
+                    <CategoryBox
+                        categoryIcon="question-circle"
+                        categoryName="Other"
+                    />
+                    <CategoryBox
+                        categoryIcon="plus-circle"
+                        categoryName="Add Category"
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+export default TransactionForm
