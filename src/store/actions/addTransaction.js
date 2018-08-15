b06fc7f08975b0f7533636db22c3a992fcb0240b
Pashan Irani <pashanirani@gmail.com>
2018-08-14 19:29:16 -0700

    rounding to 2 decimal places

--- src/store/actions/addTransaction.js
+++ src/store/actions/addTransaction.js
@@ -4,6 +4,7 @@ const ADD_TRANSACTION = 'ADD_TRANSACTION'
 export { ADD_TRANSACTION }
 
 export default transaction => {
+    transaction.amount = Math.round(transaction.amount * 100) / 100 //round to 2 decimal places
     return {
         type: ADD_TRANSACTION,
         payload: transaction
