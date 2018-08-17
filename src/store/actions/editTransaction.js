156f71484ed2be5b2086e7f37c328953cd35d346
Pashan Irani <pashanirani@gmail.com>
2018-08-17 11:10:17 -0700

    fixed

--- src/store/actions/editTransaction.js
+++ src/store/actions/editTransaction.js
@@ -6,6 +6,5 @@ export { EDIT_TRANSACTION }
 export default (updatedTransaction, id) => {
     updatedTransaction.id = id
 
-    console.log(EDIT_TRANSACTION + '[[][][[]][[][]')
     return { type: EDIT_TRANSACTION, payload: updatedTransaction }
 }
