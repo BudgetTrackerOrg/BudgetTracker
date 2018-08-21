df134657602fac17b342a9dcf56e3817a353464a
gregsakai <gregsakai@gmail.com>
2018-08-20 20:12:03 -0700

    Redux persist saves state, bug created when categories are clicked

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { Provider } from 'react-redux'
-import { configureStore, persistor } from './src/store'
+import { store, persistor } from './src/store'
 import { PersistGate } from 'redux-persist/lib/integration/react'
-
-const store = configureStore()
 
 const rootApp = () => (
     <Provider store={store}>
+        {/* TODO: Create a loading screen before the redux state is retrieved from device */}
         <PersistGate loading={<App />} persistor={persistor}>
             <App />
         </PersistGate>
