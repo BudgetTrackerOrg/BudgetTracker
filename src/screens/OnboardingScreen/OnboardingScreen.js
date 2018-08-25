d76d9a1e6de98d614c44b216da639c313077eed7
Pashan Irani <pashanirani@gmail.com>
2018-08-24 18:17:35 -0700

    started working on login button

--- src/screens/OnboardingScreen/OnboardingScreen.js
+++ src/screens/OnboardingScreen/OnboardingScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Button, Text } from 'react-native'
+import { Text, TouchableHighlight } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import { colors } from '../../globals'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'
 
 class OnboardingScreen extends Component {
     render() {
@@ -11,20 +13,39 @@ class OnboardingScreen extends Component {
                     flex: 1,
                     justifyContent: 'center',
                     alignItems: 'center',
-                    margin: 0
+                    margin: 0,
+                    padding: 40
                 }}
                 colors={colors.backgroundGradient}
             >
-                <Text>Welcome to the onboarding screen</Text>
-                <Button
-                    title="done"
+                <Icon name="cloud" size={100} color="white" />
+                <Text style={styles.header}>Backup Your Data!</Text>
+                <Text style={styles.text}>
+                    Login with your Google account, to backup your data!
+                </Text>
+                {/* <GoogleSigninButton style={{ width: 48, height: 48 }} /> */}
+                <TouchableHighlight
                     onPress={() => {
                         this.props.navigation.navigate('Home')
                     }}
-                />
+                >
+                    <Text>skip</Text>
+                </TouchableHighlight>
             </LinearGradient>
         )
     }
 }
 
+const styles = {
+    header: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#fff'
+    },
+    text: {
+        fontSize: 15,
+        textAlign: 'center',
+        color: '#fff'
+    }
+}
 export default OnboardingScreen
