3e328519ef078c9b99695e1f9c873d233395f2c8
gregsakai <gregsakai@gmail.com>
2019-02-10 19:03:34 -0800

    Fixed VectorIcon bug

--- android/app/src/main/java/com/budgettracker/MainApplication.java
+++ android/app/src/main/java/com/budgettracker/MainApplication.java
@@ -7,7 +7,6 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new RNGoogleSigninPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
